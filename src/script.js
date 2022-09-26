4a6efd7e88cedc3f96af948840e53b3c965e2080
m0ntz <109100764+m0ntz@users.noreply.github.com>
2022-09-26 21:38:54 +0200

    Display forecast data from API

--- src/script.js
+++ src/script.js
@@ -21,32 +21,53 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes} `;
 }
 
+// formate date for the forecast
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 // display forecast
-function displayForecast() {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Tue", "Wed", "Thur", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2">
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
             <div class="weather-forecast-date">
-              ${day}</div>
-            <img src="..." alt="" />
+              ${formatDay(forecastDay.dt)}</div>
+            <img src="http://openweathermap.org/img/wn/${
+              forecastDay.weather[0].icon
+            }@2x.png"/>
             <div class="weather-forecast-temp">
               <span class="weather-temp-max">
-                18°
+                ${Math.round(forecastDay.temp.max)}°
               </span>
               <span class="weather-temp-min">
-                12°
+                ${Math.round(forecastDay.temp.min)}°
               </span>
             </div>
           </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "5fb2817fa3d0764008333a7517e3e6c1";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 // display Temperature
@@ -76,6 +97,9 @@ function displayTemperature(response) {
   );
   // icon alt description
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  // coordinates data
+  getForecast(response.data.coord);
 }
 
 // search function
@@ -131,4 +155,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Düsseldorf");
-displayForecast();
